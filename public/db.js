bd382267a9aec2def8b972bf445bdbb4cefc23f0
Victoria Lee <victoriacmlee@gmail.com>
2020-12-22 14:54:57 -0500

    set up mongoose and pseudo code db.js

---
+++ public/db.js
@@ -0,0 +1,17 @@
+// create the database for entries
+
+// seed/update database with .onupgradeneeded
+
+// function check connection .onsuccess
+
+// catch error
+
+// a function take pending records entered offline
+
+// a function complete transaction POST into database once connected online
+
+    // clear items once all complete
+
+// listen to online, call function to operate transaction 
+
+
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
+// dependencies 
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// PORT setup
 const PORT = 3000;
 
+// express server
 const app = express();
 
+// logger
 app.use(logger("dev"));
 
+// middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// static files from public folder
 app.use(express.static("public"));
 
+// mongoose connection
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -23,6 +30,7 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
+// app listener on PORT
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
