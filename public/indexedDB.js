9179823661a283cd873ccc28bfb100051f762be7
Victoria Lee <victoriacmlee@gmail.com>
2020-12-22 23:04:28 -0500

    indexedDB pseudo code reformat and linked in index.html

---
+++ public/indexedDB.js
@@ -0,0 +1,30 @@
+// indexedDB setup
+
+// open budget .open and create "budget"
+
+// first load db .onupgradeneeded
+
+    // create pending schema
+
+
+// connection .onsuccess
+    
+    // if online
+        
+        // function read database
+
+// err catching .onerror
+
+
+// function read database
+
+    // get reference to db and store 
+
+        // insert everything in bulk to db 
+
+        // format response to object
+
+        // clear pending
+
+
+// listen for app online/offline
