b39bfe56d716d1d17a7f299fb0cbe6c253a62aae
Victoria Lee <victoriacmlee@gmail.com>
2020-12-22 21:58:21 -0500

    indexedDB setup

--- public/db.js
+++ public/db.js
@@ -1,10 +1,25 @@
-// create the database for entries
+let db;
+// create a database request 
+const request = indexDB.open("transaction", 1);
 
 // seed/update database with .onupgradeneeded
+request.onupgradeneeded = function (e) {
+    // set up db
+    const db = evt.target.result;
+    // set up pending data
+    db.createOnjectStore("pending", { autoincrement : true });
+};
 
 // function check connection .onsuccess
+request.onsuccess = function (e) {
+    db = e.target.result;
+
+}
 
 // catch error
+request.onerror = function(e) {
+    console.loge(event.target.errorCode)
+}
 
 // a function take pending records entered offline
 
@@ -13,5 +28,5 @@
     // clear items once all complete
 
 // listen to online, call function to operate transaction 
-
+window.addEventListener("online", insertToDataBase);
 
