9b98aecce277e4ecdc949338a748d91815c272b2
Victoria Lee <victoriacmlee@gmail.com>
2020-12-24 23:18:24 -0500

    setup indexedDB logic

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,30 +1,64 @@
 // indexedDB setup
+let db;
 
-// open budget .open and create "budget"
+// open budget .open and create "budget", version 1
+const request = indexDB.open("budget", 1)
 
-// first load db .onupgradeneeded
+// first load db .onupgradeneeded, pass variable with deconstruction
+request.onupgradeneeded = function ({ target }) {
+
+    // get db reference from data passed in function
+    let db = target.result;
 
     // create pending schema
+    db.createObjectStore("pending", { autoIncrement: true });
+};
 
 
 // connection .onsuccess
-    
+request.oncussess = function ({ target }) {
+
+    // get db reference
+    let db = target.result;
+
     // if online
-        
+    if (navidator.onLine) {
+
         // function read database
+        onlineOperation();
+    }
+
+};
 
 // err catching .onerror
+request.onerror = function ({ target }) {
 
+    // print error code
+    console.log(target.errorCode);
+}
+
+// function to store record
+function saveRecord(record) {
+
+    console.log("storing record");
+
+    // store records in pending until online
+
+};
 
 // function read database
+function onlineOperation() {
+
+    console.log("online")
 
     // get reference to db and store 
 
-        // insert everything in bulk to db 
+    // insert everything in bulk to db 
 
-        // format response to object
+    // format response to object
 
-        // clear pending
+    // clear pending
 
-
+};
 // listen for app online/offline
+window.addEventListener("online", onlineOperation)
