870733e077fe92597fe84b49dca65c199f572b7a
Victoria Lee <victoriacmlee@gmail.com>
2020-12-25 00:28:44 -0500

    setup Atlas Mongoose connection

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+// register service worker
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("service-worker.js").then(reg => {
+        console.log("We found your service worker file!", reg);
+      });
+    });
+  }
+
+// define transaction variables
 let transactions = [];
 let myChart;
 
--- server.js
+++ server.js
@@ -22,9 +22,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongoose connection
-mongoose.connect("mongodb://localhost/budget", {
+const db = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
 });
 
 // routes
