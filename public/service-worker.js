b58148a5cf41bf825733769b935eb434117a93b3
Victoria Lee <victoriacmlee@gmail.com>
2020-12-25 00:38:45 -0500

    service-worker.js setup pseudo code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,14 @@
 console.log("in service-worker.js")
+
+// setup service worker for offline experience
+const CACHE_NAME = "static-cache-v1"
+const DATA_CACHE_NAME = "data-cache-v1"
+
+// all static files
+const FILES_TO_CACHE = [ ]
+
+// install
+
+// activate
+
+// fetch
