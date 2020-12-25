625c1eeb5d28dc9075b9e2492a15e40e14c8c769
Victoria Lee <victoriacmlee@gmail.com>
2020-12-24 23:52:31 -0500

    defined onlineOperation function

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,4 +1,5 @@
 // indexedDB setup
+
 let db;
 
 // open budget .open and create "budget", version 1
@@ -19,14 +20,14 @@ request.onupgradeneeded = function ({ target }) {
 request.oncussess = function ({ target }) {
 
     // get db reference
-    let db = target.result;
+    db = target.result;
 
     // if online
     if (navidator.onLine) {
 
         // function read database
         onlineOperation();
-    }
+    };
 
 };
 
@@ -60,13 +61,41 @@ function onlineOperation() {
     console.log("online");
 
     // get reference to db and store 
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending");
+    const fetchPending = store.getAll();
 
     // insert everything in bulk to db 
+    fetchPending.oncussess = function () {
 
-    // format response to object
+        // if there are pending records (bulk)
+        if (fetchPending.result.length > 0) {
+            fetch("/api/transction/bulk", {
+                method: "POST",
+                body: JSON.stringify(fetchPending.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "content-Type": "application/json"
+                }
+            })
 
-    // clear pending
+                .then(response => {
+                    // format response to object
+                    return response.json();
+                })
 
-};
+                // clear pending
+                .then(() => {
+
+                    // get reference
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+
+                    // clear table
+                    store.clear();
+                });
+        }
+    };
+}
 // listen for app online/offline
 window.addEventListener("online", onlineOperation);
